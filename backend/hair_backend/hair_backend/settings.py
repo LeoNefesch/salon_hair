38a3764297b22a40a566d8ec7cde71c3dcec2300
leonid.negaschev <leonid.negaschev@gmail.com>
2023-07-26 16:22:16 +0500

    Hide secret key

--- backend/hair_backend/hair_backend/settings.py
+++ backend/hair_backend/hair_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%m(k_f*3lf_ub7l2x_9pbz=w-k5rcflv=%%gu$wmx15yj5by3'
+SECRET_KEY = os.getenv('SECRET_KEY', default='key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
